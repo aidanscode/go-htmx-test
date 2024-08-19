37558cbff1fa2afc94c76e5d3337bc093e1d8b7c
Aidan Murphey <amurphey7@gmail.com>
2024-08-19 09:26:12 -0400

    Learning validation - adding ability to delete a user, and ensuring only one user exists per email

--- http/server.go
+++ http/server.go
@@ -1,6 +1,7 @@
 package http
 
 import (
+	"errors"
 	"html/template"
 	"io"
 	"net/http"
@@ -29,6 +30,16 @@ func NewIndexData(users []User) *IndexData {
 	return &IndexData{Users: users}
 }
 
+type CreateUserData struct {
+	Name string
+	Email string
+	ErrorMessage string
+}
+
+func NewCreateUserData(name, email, errorMessage string) *CreateUserData {
+	return &CreateUserData{Name: name, Email: email, ErrorMessage: errorMessage}
+}
+
 func Start() {
 	server := echo.New()
 	renderer := &Renderer{
@@ -46,11 +57,40 @@ func Start() {
 	})
 
 	server.POST("/user", func(con echo.Context) error {
-		//fmt.Println(con.FormParams())
-		newUser := User{Name: con.FormValue("name"), Email: con.FormValue("email")}
+		name := con.FormValue("name")
+		email := con.FormValue("email")
+
+		_, err := findUserIdWithEmail(email, users)
+		if (err == nil) {
+			return con.Render(http.StatusUnprocessableEntity, "add-user", NewCreateUserData(name, email, "User already exists with given email"))
+		}
+
+		newUser := User{Name: name, Email: email}
 		users = append(users, newUser)
 		return con.Render(http.StatusCreated, "user", newUser)
 	})
 
+	server.DELETE("/user", func(con echo.Context) error {
+		emailToDelete := con.FormValue("email")
+		
+		userIndex, err := findUserIdWithEmail(emailToDelete, users)
+		if (err != nil) {
+			return con.HTML(http.StatusNotFound, "<span>No user found with given email</span>")
+		}
+
+		users = append(users[:userIndex], users[userIndex + 1:]...)
+		return con.HTML(http.StatusOK, "")
+	})
+
 	server.Logger.Fatal(server.Start(":8000"))
+}
+
+func findUserIdWithEmail(email string, users []User) (int, error) {
+	for index, user := range users {
+		if user.Email == email {
+			return index, nil
+		}
+	}
+
+	return 0, errors.New("User not found")
 }
